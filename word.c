530955ee8694ffbd0734910d5b325722a3768be3
mohamedIrfan3012 <42871545+mohamedIrfan3012@users.noreply.github.com>
2019-08-02 14:02:53 +0530

    Update and rename backup.c to word.c

--- word.c
+++ word.c
@@ -11,33 +11,39 @@
 #include<string.h>
 int main()
 {
-    char a[5],b[5],c[5];
+    char a[25],b[25],c[25];
+    int i;
     scanf("%s",a);
     scanf("%s",b);
     scanf("%s",c);
-    int i;
-    for(i=0;i<strlen(a);i++)
-    {
-        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
-        {
-            a[i]==$;
-        
-            
-        }
     
-        else
-        break;
+    for(i=1;i<=strlen(a);i++)
+    {
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+        {
+            a[i]='$';
+        
+            }
+            else
+            
+            {
+                break;
+            }
+    
+    
     } 
-    for(i=0;i<strlen(b);i++)
+    for(i=1;i<=strlen(b);i++)
         {
         if(b[i]!='a'&&b[i]!='e'&&b[i]!='i'&&b[i]!='o'&&b[i]!='u'&&b[i]!='A'&&b[i]!='E'&&b[i]!='I'&&b[i]!='O'&&b[i]!='U')
         {
-            b[i]==#;
+            b[i]='#';
         }
-        else
-        break;
+        else{
+            break;
+        }
+        
       }
-      for(i=1;i<=strlen(c);i++)
+      for(i=0;i<=strlen(c);i++)
         {
             if(c[i]>='a'&&c[i]<='z')
            {
@@ -45,5 +51,6 @@ int main()
             
         }
     }    printf("%s%s%s",a,b,c);
+    return 0;
     
 }
